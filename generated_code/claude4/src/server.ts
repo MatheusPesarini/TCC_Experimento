f2c62e8f662574ad19282d979baa5cf2e012e245
Matheus Pesarini <mrogeriopesarini@gmail.com>
2025-08-22 16:28:40 -0300

    Do zero

--- generated_code/claude4/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { buildApp } from './app';
-
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3002;
-const app = buildApp();
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`claude4 API listening on port ${PORT}`);
-});
