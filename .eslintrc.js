5f2eaa97e7d468e6b1e2d60ba798d9e260c10008
Matheus Pesarini <mrogeriopesarini@gmail.com>
2025-08-24 13:30:58 -0300

    chore: initialize package.json with basic project configuration and devDependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,33 @@
 module.exports = {
+    root: true,
     env: {
-        browser: true,
         es2021: true,
+        node: true,
     },
-    extends: [
-        'eslint:recommended',
-    ],
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
+    extends: ['eslint:recommended'],
+    overrides: [
+        {
+            files: ['GPT5/**/src/**/*.{ts,tsx}', 'Claude4/**/src/**/*.{ts,tsx}'],
+            parser: '@typescript-eslint/parser',
+            plugins: ['@typescript-eslint', 'sonarjs'],
+            extends: [
+                'eslint:recommended',
+                'plugin:@typescript-eslint/recommended',
+                'plugin:sonarjs/recommended',
+            ],
+        },
+        {
+            files: ['**/*.test.{js,ts}'],
+            env: { jest: true },
+        },
+    ],
     rules: {
+        'no-unused-vars': 'warn',
+        'no-console': 'off',
+        'sonarjs/cognitive-complexity': ['warn', 15],
     },
 };
